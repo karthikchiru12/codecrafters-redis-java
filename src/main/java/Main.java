3fdd4f3167b0b72e54914cf647ba3438b2dc9d78
Chiranjeevi Karthik <karthikchiru12@gmail.com>
2023-08-13 14:22:53 +0530

    Stage 7, Implementing the expiry time, v1

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -19,7 +19,7 @@ public class Main {
             Map<String, List<String>> redisStore = new HashMap<>();
 
             while (true) {
-                Client client = new Client(serverSocket.accept(),redisStore);
+                Client client = new Client(serverSocket.accept(), redisStore);
                 client.start();
             }
 
--- src/main/java/helper/Client.java
+++ src/main/java/helper/Client.java
@@ -5,12 +5,10 @@ import java.io.DataOutputStream;
 import java.io.IOException;
 import java.io.InputStreamReader;
 import java.net.Socket;
-import java.text.SimpleDateFormat;
 import java.util.ArrayList;
 import java.util.Date;
 import java.util.List;
 import java.util.Map;
-import java.util.stream.Collectors;
 
 public class Client extends Thread {
 
@@ -18,7 +16,7 @@ public class Client extends Thread {
 
     private Map<String, List<String>> redisStore;
 
-    public Client(Socket clientSocket, Map<String,List<String>> redisStore) {
+    public Client(Socket clientSocket, Map<String, List<String>> redisStore) {
         this.clientSocket = clientSocket;
         this.redisStore = redisStore;
     }
@@ -38,16 +36,14 @@ public class Client extends Thread {
                         dataOutputStream.writeBytes("+PONG\r\n");
                         dataOutputStream.flush();
                     }
-                    if(line.equals("echo"))
-                    {
+                    if (line.equals("echo")) {
                         String lengthOfString = bufferedReader.readLine();
                         String echoString = bufferedReader.readLine();
-                        System.out.println(lengthOfString+echoString);
-                        dataOutputStream.writeBytes("+"+echoString+"\r\n");
+                        System.out.println(lengthOfString + echoString);
+                        dataOutputStream.writeBytes("+" + echoString + "\r\n");
                         dataOutputStream.flush();
                     }
-                    if(line.equals("set"))
-                    {
+                    if (line.equals("set")) {
                         List<String> setList = new ArrayList<>();
                         String keyLength = bufferedReader.readLine();
                         String keyString = bufferedReader.readLine();
@@ -58,47 +54,35 @@ public class Client extends Thread {
                         setList.add(valueString);
 
                         String args;
-                        if((args = bufferedReader.readLine())!=null)
-                        {
-                            if(args == "px")
-                            {
+                        if ((args = bufferedReader.readLine()) != null) {
+                            if (args.equals("px")) {
                                 String expiryInSeconds = bufferedReader.readLine();
                                 Date date = new Date();
-                                Long expiryTime = date.getTime() + Long.parseLong(expiryInSeconds);
-                                setList.add(expiryTime.toString());
+                                long expiryTime = date.getTime() + Long.parseLong(expiryInSeconds);
+                                setList.add(Long.toString(expiryTime));
                             }
-                        }
-                        else
-                        {
-                            setList.add(null);
+                        } else {
+                            setList.add("0");
                         }
 
-                        this.redisStore.put(keyString,setList);
+                        this.redisStore.put(keyString, setList);
 
                         dataOutputStream.writeBytes("+OK\r\n");
                         dataOutputStream.flush();
                     }
-                    if(line.equals("get"))
-                    {
+                    if (line.equals("get")) {
                         String keyLength = bufferedReader.readLine();
                         String keyString = bufferedReader.readLine();
 
-                        Long currentTime = new Date().getTime();
-                        Long expiryTime = Long.parseLong(this.redisStore.get(keyString).get(1));
-                        if(expiryTime != null)
-                        {
-                            if(currentTime > expiryTime)
-                            {
-                                dataOutputStream.writeBytes("$0\r\n\r\n");
-                                dataOutputStream.flush();
-                            }
-                            else
-                            {
-                                dataOutputStream.writeBytes("+"+this.redisStore.get(keyString).get(0)+"\r\n");
-                                dataOutputStream.flush();
-                            }
+                        long currentTime = new Date().getTime();
+                        long expiryTime = Long.parseLong(this.redisStore.get(keyString).get(1));
+                        if (expiryTime != 0 || currentTime > expiryTime) {
+                            dataOutputStream.writeBytes("$0\r\n\r\n");
+                            dataOutputStream.flush();
+                        } else {
+                            dataOutputStream.writeBytes("+" + this.redisStore.get(keyString).get(0) + "\r\n");
+                            dataOutputStream.flush();
                         }
-
 
 
                     }
